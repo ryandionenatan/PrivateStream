e94ccbcb158bbff282f68901bf61b784addccb9f
ryandionenatan <177109050+ryandionenatan@users.noreply.github.com>
2025-01-31 08:55:15 +0700

    Update otor.php

--- adm/otor.php
+++ adm/otor.php
@@ -1,5 +1,5 @@
 <?php
-$link=mysqli_connect("dbaas-db-3534850-do-user-16515205-0.c.db.ondigitalocean.com","doadmin","AVNS_z7C1c6HDbgnmPh8zck_","defaultdb","25060");
+$link=mysqli_connect("host","user","password","db","port");
 
 if (mysqli_connect_error()) {
 $logMessage = 'MySQL Error: ' . mysqli_connect_error();
